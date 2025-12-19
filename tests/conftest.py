ea27c112093d2a08caca8689423c0588ba39b5fb
Victor Val <vivaldionder@gmail.com>
2025-12-19 16:00:24 +0300

    browser choice added in fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,29 @@
 import pytest
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru',
                      help="Choose language")
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        browser = Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = Chrome(options=options)
+    elif browser_name == 'firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+
     browser.implicitly_wait(10)
     yield browser
     browser.quit()
